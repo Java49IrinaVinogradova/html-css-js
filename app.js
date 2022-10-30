404b22215ea3155d4dfa7a0ecdaac0ca376a8fe9
Irina <vinogradova8998@gmail.com>
2022-10-30 17:02:39 +0200

    js lesson #1

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Hello world");
+ for( i = 0; i < 3; i++) {
+     setTimeout(function(){
+         console.log(i)
+     });
+ }
+ i = 100;
+ for (let i = 0; i < 3; i++) {
+     setTimeout(function () { console.log(i) });
+ } 
+
+ for (var i = 0; i < 3; i++) {
+      console.log(i) ;
+ }
+ function sum(op1, op2) {
+     let res = op1 + op2;
+     return res;
+ }
+ let op1 = 10;
+ let op2 = 20;
+ let res = sum(op1, op2);
+ console.log(res)
